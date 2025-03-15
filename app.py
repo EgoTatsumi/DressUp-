2bc9c1185dc41c406cc42cd679a205ff5e988089
piterbsj <aleksandracarova057@gmail.com>
2025-03-15 19:58:35 +0300

    главная страница

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main_page():
-
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(port=8080, host="127.0.0.1")
