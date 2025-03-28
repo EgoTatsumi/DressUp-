b23bc687a65e9e7cb3f6b292948880986d82a76f
piterbsj <aleksandracarova057@gmail.com>
2025-03-28 23:34:48 +0300

    вход в аккаунт и карточка товара.

--- app.py
+++ app.py
@@ -3,9 +3,25 @@ from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/")               #главная страница
 def main_page():
     return render_template('index.html')
+
+@app.route("/cart")           #корзина
+def cart_page():
+    return render_template('cart.html')
+
+@app.route('/profile')
+def profile_page():
+    pass
+
+@app.route('/demo')          #демонстрация товара
+def demo_page():
+    return render_template('demo.html')
+
+@app.route('/sign_in')       #регистрация
+def sign_in():
+    return render_template('base.html')
 
 if __name__ == "__main__":
     app.run(port=8080, host="127.0.0.1")
